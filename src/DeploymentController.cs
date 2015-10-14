127e57bee476fff5548314073461f9c8e8d234c2
winminoo <oo.winmin@gmail.com>
2015-10-14 15:46:03 +1100

    Fix Bug for Cursor
    Cursor  Grid item now place the correct position

--- src/DeploymentController.cs
+++ src/DeploymentController.cs
@@ -106,8 +106,11 @@ static class DeploymentController
 		//Calculate the row/col clicked
 		int row = 0;
 		int col = 0;
-		row = Convert.ToInt32(Math.Floor((mouse.Y) / (CELL_HEIGHT + CELL_GAP)));
+		row = Convert.ToInt32(Math.Floor((mouse.Y - FIELD_TOP) / (CELL_HEIGHT + CELL_GAP)));
 		col = Convert.ToInt32(Math.Floor((mouse.X - FIELD_LEFT) / (CELL_WIDTH + CELL_GAP)));
+
+		//row = Convert.ToInt32(Math.Floor((mouse.Y) / (CELL_HEIGHT + CELL_GAP)));
+		//col = Convert.ToInt32(Math.Floor((mouse.X - FIELD_LEFT) / (CELL_WIDTH + CELL_GAP)));
 
 		if (row >= 0 & row < HumanPlayer.PlayerGrid.Height) {
 			if (col >= 0 & col < HumanPlayer.PlayerGrid.Width) {
